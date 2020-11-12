b0076d2ee672248e00039ccf51ffb250ff63d16d
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-12 17:39:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ class Tree {
 }
 
 //Deciduous
+class Deciduous extends Tree {
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
+
+    static definition() {
+        return super.definition() + ' Deciduous trees shed their leaves annually.'
+    }
+}
 
 
 //Evergreen
