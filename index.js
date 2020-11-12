e75459636b3036b7331d8be739705924292696f7
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-12 17:38:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Write your classes here
 //Tree
+class Tree {
+  constructor( species ) {
+    this.species = species
+  }
 
+  static definition() {
+    return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
+  }
+}
 
 //Deciduous
 
